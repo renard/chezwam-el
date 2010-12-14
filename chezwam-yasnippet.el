37eed84bc0782417b493b28bada608d1fa3fed2f
Sébastien Gross <sebchezwamorg>
2010-12-14 15:55:13 +0100

    Add yasnippet customization.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

---
+++ chezwam-yasnippet.el
@@ -0,0 +1,52 @@
+;;; chezwam-yasnippet.el --- YASnippet customisation.
+
+;; Copyright © 2010 Sebastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
+
+;; Author: Sebastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
+;; Keywords: emacs, yasnippet, configuration
+;; Created: 2010-12-09
+;; Last changed: 2010-12-10 12:18:17
+;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
+
+;; This file is NOT part of GNU Emacs.
+
+;;; Commentary:
+;; 
+
+
+;;; Code:
+
+(require 'yasnippet)
+
+(defcustom cw:yasnippet:new-file-template "new-file-tpl"
+  "Yasnippet template to use when creating a new file using
+`cw:yasnippet:insert-snippet-new-file'"
+  :type 'string
+  :group 'chezwam)
+
+(defun cw:yasnippet:insert-snippet-new-file ()
+  "Insert a default file skeleton when a new file is created as defined
+by `cw:yasnippet:new-file-template'.
+
+To make this tip work, a \"NEWFILE-TEMPLATE.yasnippet\" file
+should contain the default file skeleton."
+  (interactive)
+  (when (and (buffer-file-name)
+	     (not (file-exists-p (buffer-file-name)))
+	     (= (point-max) 1))
+    (insert cw:yasnippet:new-file-template)
+    (yas/expand)))
+
+;; workarround for yasnippet to act as a skeleton creator.
+(setq-default yas/dont-activate
+	      #'(lambda ()
+		  (and yas/snippet-dirs
+		       (null (yas/get-snippet-tables)))))
+
+(setq yas/snippet-dirs "~/.emacs.d/templates")
+(setq yas/prompt-functions '(yas/completing-prompt))
+(yas/initialize)
+(yas/load-directory yas/root-directory)
+(add-hook 'yas/minor-mode-hook 'cw:yasnippet:insert-snippet-new-file)
+
+(provide 'chezwam-yasnippet)
