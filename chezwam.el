1aa8d6a4239a5a447f00e12296767588b89e73c9
Sébastien Gross <sebchezwamorg>
2011-03-30 15:25:37 +0200

    Add global module.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

---
+++ chezwam.el
@@ -0,0 +1,28 @@
+;;; chezwam.el --- Set visual effects for Emacs
+
+;; Copyright © 2010 Sebastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
+
+;; Author: Sebastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
+;; Keywords: emacs, configuration
+;; Created: 2010-12-09
+;; Last changed: 2011-03-28 00:19:20
+;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
+
+;; This file is NOT part of GNU Emacs.
+
+;;; Commentary:
+;;
+
+(require 'chezwam-macros)
+(require 'chezwam-emacs)
+(require 'chezwam-buffer)
+(require 'chezwam-doc-mode)
+(require 'chezwam-ibuffer)
+(require 'chezwam-keybindings)
+(require 'chezwam-email)
+(require 'chezwam-dired)
+(require 'chezwam-tramp)
+(require 'chezwam-term)
+(require 'chezwam-org)
+
+(provide 'chezwam)
